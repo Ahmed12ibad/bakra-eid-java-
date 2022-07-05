ea825ba78cc699ea117bb7f08a3a3355bb49de8f
Ahmed12ibad <ahmedhamid777fast@gmail.com>
2022-07-05 12:45:33 +0500

    ahmed

--- cheak.js
+++ cheak.js
@@ -73,5 +73,5 @@ card2.innerHTML="<h1> second eid <h1/>"+eid2 +" day "+eidH2+"hours"+"<br>"+day2
 var card3=document.getElementById("card-3")
 
 card3.innerHTML="<h1> third eid <h1/>"+  eid3+ " day "+eidH3+"hours"+"<br>"+day3
-
+//
 
